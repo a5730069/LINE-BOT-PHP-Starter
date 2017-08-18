3b3c66ce6dd43837b0e9a025c17c543e51415c05
a5730069 <a5730069@gmail.com>
2017-08-18 16:22:46 +0700

    Update pub.php

--- pub.php
+++ pub.php
@@ -7,17 +7,21 @@
   }
   function getMqttfromlineMsg($lineMsg){
  
-    $pos = strpos($lineMsg, ":");
-    if($pos){
-      $splitMsg = explode(":", $lineMsg);
-      $topic = $splitMsg[0];
-      $msg = $splitMsg[1];
-      pubMqtt($topic,$msg);
-    }else{
-      $topic = "NodeMCU1";
-      $msg = $lineMsg;
-      pubMqtt($topic,$msg);
-    }
+
+  $request = "auth=jJhs4OCpicDIGTT:kWNwUoBoEZenL50J2h8Th8XGT";
+  $urlWithoutProtocol = "https://api.netpie.io/microgear/jadetest/NodeMCU1?retain";
+  $isRequestHeader = FALSE;
+   
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $urlWithoutProtocol);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $lineMsg);
+  curl_setopt($ch, CURLOPT_USERPWD, "jJhs4OCpicDIGTT:kWNwUoBoEZenL50J2h8Th8XGT");
+  $productivity = curl_exec($ch);
+  curl_close($ch);
+  echo $productivity;
   }
  
   function put($url,$tmsg)
