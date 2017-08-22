7567121fa18394d4161b23432597415716b18d9e
a5730069 <a5730069@gmail.com>
2017-08-23 02:07:47 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,44 +1,33 @@
 <?php
- require("pub.php");
 $access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
-$content = file_get_contents('php://input');
+//$content = file_get_contents('php://input');
 // Parse JSON
-$events = json_decode($content, true);
+//$events = json_decode($content, true);
 // Validate parsed JSON data
-if (!is_null($events['events'])) {
+//if (!is_null($events['events'])) {
 	// Loop through each event
-	foreach ($events['events'] as $event) {
+	//foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+		//if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			$text = $event['message']['text'];
+			//$text = $event['message']['text'];
 			// Get replyToken
-			$replyToken = $event['replyToken'];
+			//$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-
-			 $pos = strpos($text, ":");
-			    if($pos){
-			      $splitMsg = explode(":", $text);
-			      $topic = $splitMsg[0];
-			      $msg = $splitMsg[1];
-			      getMqttfromlineMsg($msg);
-			    }else{
-				getMqttfromlineMsg($text);
-			    }
-			
-
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => '555'
+				//'text' => $text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
+			$url = 'https://api.line.me/v2/bot/message/push';
 			$data = [
-				'replyToken' => $replyToken,
+
+				'to' => 'Ue77a191627f6ac91899e75d92264310c',
 				'messages' => [$messages],
 			];
 			$post = json_encode($data);
@@ -54,7 +43,7 @@ if (!is_null($events['events'])) {
 			curl_close($ch);
 
 			echo $result . "\r\n";
-		}
-	}
-}
+		//}
+	//}
+//}
 echo "OK";
