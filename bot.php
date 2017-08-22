ed9f548ae445b4717029d697848a704c5ad48fd7
a5730069 <a5730069@gmail.com>
2017-08-23 02:34:36 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,5 +1,7 @@
 <?php
  require("pub.php");
+$access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU=';
+
 
  if( !is_null($_GET['name'])){
 	 echo "pass";
@@ -30,8 +32,6 @@
 
 			echo $result . "\r\n";
 }
-$access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU=';
-
 
 // Get POST body content
 $content = file_get_contents('php://input');
