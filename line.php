25dfe4c73afa3a47d9977ce5aa0822cd7083b73d
a5730069 <a5730069@gmail.com>
2017-08-24 02:59:41 +0700

    Update line.php

--- line.php
+++ line.php
@@ -1,10 +1,11 @@
  <?php
-  $access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU=';
+  
 
 function send_LINE($msg){
+  $access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU=';
   $messages = [
         'type' => 'text',
-        'text' => '555'
+        'text' => $msg
         //'text' => $text
       ];
 
@@ -28,6 +29,5 @@ function send_LINE($msg){
       curl_close($ch);
 
       echo $result . "\r\n"; 
- echo "3";
 }
 ?>
