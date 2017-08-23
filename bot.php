045235801ec001813ed235358f60c4d622f35eeb
a5730069 <a5730069@gmail.com>
2017-08-24 00:09:03 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -89,8 +89,33 @@ echo "OK";
 
 if (!is_null($events['sensorType'])) {
 	echo "json pass";
-	send_data_LINE($events['sensorType']);
-		
+	//send_data_LINE($events['sensorType']);
+		$messages = [
+				'type' => 'text',
+				'text' => $events['sensorType']
+				//'text' => $text
+			];
+
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/push';
+			$data = [
+
+				'to' => 'Ue77a191627f6ac91899e75d92264310c',
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
 		//}
 	//}
 //}
