a88ead55b6afb43782182e89d9b3b2260d980b15
a5730069 <a5730069@gmail.com>
2017-08-23 23:53:26 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -2,10 +2,10 @@
  require("pub.php");
 $access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU=';
 
-function send_data_LINE($data){
+function send_data_LINE($text){
 	$messages = [
 				'type' => 'text',
-				'text' => $data
+				'text' => $text
 				//'text' => $text
 			];
 
@@ -89,7 +89,8 @@ $events = json_decode($content, true);
 // Validate parsed JSON data
 if (!is_null($events['sensorType'])) {
 	echo "json pass";
-	send_data_LINE($events['sensorType']);
+	$text = $events['sensorType'];
+	send_data_LINE($text);
 		
 	}
 if (!is_null($events['events'])) {
